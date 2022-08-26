44eca0723a5b7268e94f7db82dbf7dfd418618c7
Nikola Sirmov <sirmov0213@gmail.com>
2022-08-26 17:06:51 +0300

    Date only converter and comparer

---
+++ src/Data/HospitalityManagementSystem.Data.Common/DateOnlyComparer.cs
@@ -0,0 +1,14 @@
+﻿namespace HospitalityManagementSystem.Data.Common
+{
+    using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+    public class DateOnlyComparer : ValueComparer<DateOnly>
+    {
+        public DateOnlyComparer()
+            : base
+            (
+                (d1, d2) => d1.DayNumber == d2.DayNumber,
+                d => d.GetHashCode()
+            ) { }
+    }
+}
