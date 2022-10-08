00ebbf8d523fb9fbc9d03c879dd52b5abcf14e76
Nikola Sirmov <sirmov0213@gmail.com>
2022-10-08 14:07:45 +0300

    Added method overloads with search predicate

--- src/Data/Roomed.Data.Common/Repositories/IDeletableEntityRepository.cs
+++ src/Data/Roomed.Data.Common/Repositories/IDeletableEntityRepository.cs
@@ -1,7 +1,7 @@
 ﻿namespace Roomed.Data.Common.Repositories
 {
     using System.Linq;
-
+    using System.Linq.Expressions;
     using Roomed.Data.Common.Models;
 
     public interface IDeletableEntityRepository<TEntity, TKey> : IRepository<TEntity, TKey>
@@ -9,7 +9,11 @@
     {
         IQueryable<TEntity> AllWithDeleted();
 
+        IQueryable<TEntity> AllWithDeleted(Expression<Func<TEntity, bool>> search);
+
         IQueryable<TEntity> AllWithDeletedAsNoTracking();
+
+        IQueryable<TEntity> AllWithDeletedAsNoTracking(Expression<Func<TEntity, bool>> search);
 
         void HardDelete(TEntity entity);
 
