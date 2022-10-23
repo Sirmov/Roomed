ad70820bf9a4ab6090c8e00866a346580eff2e06
Nikola Sirmov <sirmov0213@gmail.com>
2022-10-23 21:13:16 +0300

    Created base controller

---
+++ src/Web/Roomed.Web/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿namespace Roomed.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class BaseController : Controller
+    {
+    }
+}
