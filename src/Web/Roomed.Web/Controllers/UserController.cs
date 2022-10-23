da2e1899a693cbd9f8d7c08f0f9e5ec03a00e8ed
Nikola Sirmov <sirmov0213@gmail.com>
2022-10-23 22:03:56 +0300

    Create UserController.cs

---
+++ src/Web/Roomed.Web/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿namespace Roomed.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
