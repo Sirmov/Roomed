4e9d2f0dec81ed351b15fbf31f003221c770dee0
Nikola Sirmov <sirmov0213@gmail.com>
2022-10-26 12:14:58 +0300

    Typed login view
    LoginViewModel

---
+++ src/Web/Roomed.Web.ViewModels/User/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Roomed.Web.ViewModels.User
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class LoginViewModel
+    {
+        [DataType(DataType.Text)]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
--- src/Web/Roomed.Web/Controllers/UserController.cs
+++ src/Web/Roomed.Web/Controllers/UserController.cs
@@ -1,12 +1,15 @@
 ﻿namespace Roomed.Web.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using Roomed.Web.ViewModels.User;
 
     public class UserController : Controller
     {
         public IActionResult Login()
         {
-            return View();
+            var model = new LoginViewModel();
+
+            return View(model);
         }
     }
 }
