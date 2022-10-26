e5765ad8e51e8d4072edb10309c01012e9fa5ff5
Nikola Sirmov <sirmov0213@gmail.com>
2022-10-26 15:30:55 +0300

    Create ApplicationUserConfiguration.cs

---
+++ src/Data/Roomed.Data/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace Roomed.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using Roomed.Data.Models;
+
+    using static Roomed.Common.DataConstants.ApplicationUser;
+
+    public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.Property(au => au.UserName)
+                .HasMaxLength(UserNameMaxLength)
+                .IsRequired(true);
+
+            builder.Property(au => au.Email)
+                    .HasMaxLength(EmailMaxLength)
+                    .IsRequired(true);
+
+            builder.HasIndex(au => au.UserName)
+                    .IsUnique();
+
+            builder.HasIndex(au => au.Email)
+                    .IsUnique();
+        }
+    }
+}
