68b8eab649e1cebee57fd81099c54aea13c0925e
Nikola Sirmov <sirmov0213@gmail.com>
2022-10-26 15:34:58 +0300

    Controller and actions constants

---
+++ src/Roomed.Common/ControllersActionsConstants.cs
@@ -0,0 +1,23 @@
+﻿namespace Roomed.Common
+{
+    public static class ControllersActionsConstants
+    {
+        public static class Controllers
+        {
+            public const string Home = "Home";
+            public const string User = "User";
+        }
+
+        public static class Actions
+        {
+            // Common actions
+            public const string Index = "Index";
+
+            // User controller actions
+            public const string Login = "Login";
+            public const string Register = "Register";
+            public const string Logout = "Logout";
+            public const string ForgotPassword = "ForgotPassword";
+        }
+    }
+}
