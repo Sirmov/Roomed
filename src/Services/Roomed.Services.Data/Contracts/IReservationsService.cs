5a64839bf907ec8305d64762d73900bb6b750c1a
Nikola Sirmov <sirmov0213@gmail.com>
2022-10-28 22:09:16 +0300

    Reservations service

---
+++ src/Services/Roomed.Services.Data/Contracts/IReservationsService.cs
@@ -0,0 +1,50 @@
+﻿namespace Roomed.Services.Data.Contracts
+{
+    using Roomed.Data.Models;
+
+    /// <summary>
+    /// This interface is used to abstract and document the reservations service functionality.
+    /// </summary>
+    public interface IReservationsService
+    {
+        /// <summary>
+        /// This method asynchronously returns a collection of all reservations.
+        /// </summary>
+        /// <returns>Returns a collection of <see cref="Reservation"/> objects.</returns>
+        public Task<ICollection<Reservation>> GetAllAsync();
+
+        /// <summary>
+        /// This method asynchronously returns a single reservation.
+        /// </summary>
+        /// <param name="id">The id of the reservation.</param>
+        /// <returns>Returns a single <see cref="Reservation"/> object.</returns>
+        public Task<Reservation> GetAsync(string id);
+
+        /// <summary>
+        /// This method asynchronously returns all arriving reservations on a given date.
+        /// </summary>
+        /// <param name="date">The date of the reservation.</param>
+        /// <returns>Returns a collection <see cref="Reservation"/> objects.</returns>
+        public Task<ICollection<Reservation>> GetAllArrivingFromDateAsync(DateOnly date);
+
+        /// <summary>
+        /// This method asynchronously returns all in house reservations on a given date.
+        /// </summary>
+        /// <param name="date">The date of the reservations.</param>
+        /// <returns>Returns a collection <see cref="Reservation"/> objects.</returns>
+        public Task<ICollection<Reservation>> GetAllInHouseFromDateAsync(DateOnly date);
+
+        /// <summary>
+        /// This method asynchronously returns all departing reservations on a given date.
+        /// </summary>
+        /// <param name="date">The date of the reservations.</param>
+        /// <returns>Returns a collection <see cref="Reservation"/> objects.</returns>
+        public Task<ICollection<Reservation>> GetAllDepartingFromDateAsync(DateOnly date);
+
+        /// <summary>
+        /// This method asynchronously create a <see cref="Reservation" entity in the database./>
+        /// </summary>
+        /// <returns>Returns a <see cref="Task"/>.</returns>
+        public Task CreateReservationAsync();
+    }
+}
---
+++ src/Services/Roomed.Services.Data/ReservationsService.cs
@@ -0,0 +1,34 @@
+﻿namespace Roomed.Services.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Roomed.Data.Models;
+    using Roomed.Services.Data.Contracts;
+
+    /// <summary>
+    /// This class is a implementation of the <see cref="IReservationsService"/> interface.
+    /// </summary>
+    /// <inheritdoc cref="IReservationsService"/>
+    public class ReservationsService : IReservationsService
+    {
+        /// <inheritdoc />
+        public Task CreateReservationAsync() => throw new NotImplementedException();
+
+        /// <inheritdoc />
+        public Task<ICollection<Reservation>> GetAllArrivingFromDateAsync(DateOnly date) => throw new NotImplementedException();
+
+        /// <inheritdoc />
+        public Task<ICollection<Reservation>> GetAllAsync() => throw new NotImplementedException();
+
+        /// <inheritdoc />
+        public Task<ICollection<Reservation>> GetAllDepartingFromDateAsync(DateOnly date) => throw new NotImplementedException();
+
+        /// <inheritdoc />
+        public Task<ICollection<Reservation>> GetAllInHouseFromDateAsync(DateOnly date) => throw new NotImplementedException();
+
+        /// <inheritdoc />
+        public Task<Reservation> GetAsync(string id) => throw new NotImplementedException();
+    }
+}
