8eee34b1649991a1fc15b0aa3a1190b7b3f9744a
Nikola Sirmov <sirmov0213@gmail.com>
2022-11-02 10:11:39 +0200

    Added new json service

---
+++ src/Roomed.Services.Json/Converters/NullableDateOnlyJsonConverter.cs
@@ -0,0 +1,33 @@
+﻿namespace Roomed.Services.Json.Converters
+{
+    using System.Globalization;
+
+    using Newtonsoft.Json;
+
+    public class NullableDateOnlyJsonConverter : JsonConverter<DateOnly?>
+    {
+        private const string DateFormat = "yyyy-MM-dd";
+
+        public override DateOnly? ReadJson(JsonReader reader, Type objectType, DateOnly? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            string value = (string)reader.Value;
+
+            if (value != null)
+            {
+                return DateOnly.ParseExact(value, DateFormat, CultureInfo.InvariantCulture);
+            }
+
+            return null;
+        }
+
+        public override void WriteJson(JsonWriter writer, DateOnly? value, JsonSerializer serializer)
+        {
+            if (value.HasValue)
+            {
+                writer.WriteValue(value.Value.ToString(DateFormat, CultureInfo.InvariantCulture));
+            }
+
+            writer.WriteValue(value.ToString());
+        }
+    }
+}
---
+++ src/Roomed.Services.Json/SerializerSettings/DateOnlyJsonSettings.cs
@@ -0,0 +1,22 @@
+﻿namespace Roomed.Services.Json.SerializerSettings
+{
+    using Newtonsoft.Json;
+    using Roomed.Services.Json.Converters;
+
+    public class DateOnlyJsonSettings
+    {
+        public DateOnlyJsonSettings()
+        {
+            this.Settings = new JsonSerializerSettings()
+            {
+                Converters = new List<JsonConverter>()
+                {
+                    new NullableDateOnlyJsonConverter(),
+                    new DateOnlyJsonConverter(),
+                }
+            };
+        }
+
+        public JsonSerializerSettings Settings { get; }
+    }
+}
