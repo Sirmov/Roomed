26f3d5bc6b4fdff634167676361f477d1751f08c
Nikola Sirmov <75629862+Sirmov@users.noreply.github.com>
2022-11-02 10:22:36 +0200

    Clean sandbox

--- src/Tests/Sandbox/Program.cs
+++ src/Tests/Sandbox/Program.cs
@@ -6,26 +6,6 @@ internal class Program
 {
     private static  void Main(string[] args)
     {
-        string jsonPath = "./Seeding/IdentityDocumentSeed.json";
-
-        string text = File.ReadAllText(jsonPath);
-
-        IEnumerable<IdentityDocument> data = JsonConvert.DeserializeObject<IEnumerable<IdentityDocument>>(text, new DateOnlyJsonConverter());
+        
     }
-
-    public class DateOnlyJsonConverter : JsonConverter<DateOnly>
-    {
-        private const string DateFormat = "yyyy-MM-dd";
-
-        public override DateOnly ReadJson(JsonReader reader, Type objectType, DateOnly existingValue, bool hasExistingValue, JsonSerializer serializer)
-        {
-            return DateOnly.ParseExact((string)reader.Value, DateFormat, CultureInfo.InvariantCulture);
-        }
-
-        public override void WriteJson(JsonWriter writer, DateOnly value, JsonSerializer serializer)
-        {
-            writer.WriteValue(value.ToString(DateFormat, CultureInfo.InvariantCulture));
-        }
-    }
-
 }
