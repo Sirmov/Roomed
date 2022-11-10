31df615b75dd213fdc7fa69e0f59ee43f87d9367
Nikola Sirmov <sirmov0213@gmail.com>
2022-11-10 22:17:47 +0200

    Reading and displaying arriving reservations

--- src/Web/Roomed.Web/Controllers/HomeController.cs
+++ src/Web/Roomed.Web/Controllers/HomeController.cs
@@ -1,25 +1,35 @@
 ﻿namespace Roomed.Web.Controllers
 {
     using System.Diagnostics;
-
+    using AutoMapper;
     using Microsoft.AspNetCore.Mvc;
-
+    using Roomed.Data.Models;
+    using Roomed.Services.Data.Contracts;
     using Roomed.Web.ViewModels;
+    using Roomed.Web.ViewModels.Reservation;
 
     public class HomeController : BaseController
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IReservationsService reservationsService;
+        private readonly IMapper mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IReservationsService reservationsService, IMapper mapper)
         {
-            _logger = logger;
+            this.reservationsService = reservationsService;
+            this.mapper = mapper;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
+            // For testing purposes
+            //var currentDate = DateOnly.FromDateTime(DateTime.Now);
+            var currentDate = DateOnly.FromDateTime(new DateTime(2022, 12, 12));
+            var reservations = await this.reservationsService
+                .GetAllArrivingFromDateAsync(currentDate);
+            var model = reservations.Select(r => mapper.Map<ReservationViewModel>(r));
 
+            return View(model);
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
