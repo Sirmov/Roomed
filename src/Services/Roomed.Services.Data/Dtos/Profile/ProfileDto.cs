aa18d9a1c010652f2a6ea9b82f99c30212c1b914
Nikola Sirmov <sirmov0213@gmail.com>
2022-11-13 21:18:45 +0200

    More inforamtion about a profile

--- src/Services/Roomed.Services.Data/Dtos/Profile/ProfileDto.cs
+++ src/Services/Roomed.Services.Data/Dtos/Profile/ProfileDto.cs
@@ -1,5 +1,6 @@
 ﻿namespace Roomed.Services.Data.Dtos.Profile
 {
+    using Roomed.Data.Models.Enums;
     using Roomed.Services.Mapping;
 
     public class ProfileDto : IMapFrom<Roomed.Data.Models.Profile>
@@ -8,6 +9,16 @@
 
         public string FirstName { get; set; } = null!;
 
+        public string? MiddleName { get; set; }
+
         public string LastName { get; set; } = null!;
+
+        public DateOnly? Birthdate { get; set; }
+
+        public Gender? Gender { get; set; }
+
+        public string? Nationality { get; set; }
+
+        public string? Address { get; set; }
     }
 }
--- src/Web/Roomed.Web.ViewModels/Profile/ProfileViewModel.cs
+++ src/Web/Roomed.Web.ViewModels/Profile/ProfileViewModel.cs
@@ -1,14 +1,28 @@
 ﻿namespace Roomed.Web.ViewModels.Profile
 {
+    using Roomed.Data.Models.Enums;
     using Roomed.Services.Data.Dtos.Profile;
     using Roomed.Services.Mapping;
 
+    /// <summary>
+    /// This is a <see cref="Roomed.Data.Models.Profile"/> view model.
+    /// </summary>
     public class ProfileViewModel : IMapFrom<ProfileDto>
     {
         public Guid Id { get; set; }
 
         public string FirstName { get; set; } = null!;
 
+        public string? MiddleName { get; set; }
+
         public string LastName { get; set; } = null!;
+
+        public DateOnly? Birthdate { get; set; }
+
+        public Gender? Gender { get; set; }
+
+        public string? Nationality { get; set; }
+
+        public string? Address { get; set; }
     }
 }
