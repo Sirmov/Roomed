7a81f49c399bcf6468eadde4b6a243b561c76d42
Nikola Sirmov <sirmov0213@gmail.com>
2022-11-14 09:33:55 +0200

    Create actions

--- src/Web/Roomed.Web/Controllers/IdentityDocumentsController.cs
+++ src/Web/Roomed.Web/Controllers/IdentityDocumentsController.cs
@@ -13,6 +13,7 @@
     public class IdentityDocumentsController : BaseController
     {
         private readonly IIdentityDocumentsService identityDocumentsService;
+        private readonly IProfilesService profilesService;
         private readonly IMapper mapper;
 
         /// <summary>
@@ -20,10 +21,15 @@
         /// Uses constructor injection to resolve dependencies.
         /// </summary>
         /// <param name="identityDocumentsService">The implementation of <see cref="IIdentityDocumentsService"/>.</param>
+        /// <param name="profilesService">The implementation of <see cref="IProfilesService"/>.</param>
         /// <param name="mapper">The global auto mapper.</param>
-        public IdentityDocumentsController(IIdentityDocumentsService identityDocumentsService, IMapper mapper)
+        public IdentityDocumentsController(
+            IIdentityDocumentsService identityDocumentsService,
+            IProfilesService profilesService,
+            IMapper mapper)
         {
             this.identityDocumentsService = identityDocumentsService;
+            this.profilesService = profilesService;
             this.mapper = mapper;
         }
 
@@ -31,12 +37,37 @@
         /// This method returns a view with all identity documents.
         /// </summary>
         /// <returns>Returns a task of <see cref="IActionResult"/>.</returns>
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var documents = await this.identityDocumentsService.GetAllAsync();
             var model = documents.Select(id => mapper.Map<IdentityDocumentViewModel>(id));
 
             return View(model);
+        }
+
+        /// <summary>
+        /// This method returns a view with a form for creating a new identity document.
+        /// </summary>
+        /// <returns>Returns a task of <see cref="IActionResult"/>.</returns>
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var model = new IdentityDocumentInputModel();
+            ViewBag.Profiles = await this.profilesService.GetAllAsync();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityDocumentInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return Ok(model);
         }
     }
 }
