2b7e4ad522db0ba7e3bf6a76ac4a32aedbc28361
Nikola Sirmov <sirmov0213@gmail.com>
2022-11-15 21:53:58 +0200

    Added isReadonly and withDeleted parameters

--- src/Data/Roomed.Data.Common/Repositories/IDeletableEntityRepository.cs
+++ src/Data/Roomed.Data.Common/Repositories/IDeletableEntityRepository.cs
@@ -2,21 +2,50 @@
 {
     using System.Linq;
     using System.Linq.Expressions;
+
     using Roomed.Data.Common.Models;
 
+    /// <summary>
+    /// This is a repository interface that aims to abstract the direct use of the database context.
+    /// This interfaced is preferred for deletable entities over the <see cref="IRepository{TEntity, TKey}"/> interface.
+    /// </summary>
+    /// <typeparam name="TEntity">This is the entity that represents the table in the database.</typeparam>
+    /// <typeparam name="TKey">This is the type of the primary key of the entity.</typeparam>
     public interface IDeletableEntityRepository<TEntity, TKey> : IRepository<TEntity, TKey>
         where TEntity : BaseDeletableModel<TKey>
     {
-        IQueryable<TEntity> AllWithDeleted();
+        /// <summary>
+        /// This method returns a expression tree with all entities.
+        /// Depending on the <paramref name="isReadonly"/> parameter the entities will or will not be tracked.
+        /// Depending on the <paramref name="withDeleted"/> parameter all or only the not deleted entities will be returned.
+        /// </summary>
+        /// <param name="isReadonly">This flag decides whether the entities should be tracked or not.</param>
+        /// <param name="withDeleted">This flag decides whether all or only the not deleted entities should be returned.</param>
+        /// <returns>Returns an unmaterialized query with the entities.</returns>
+        IQueryable<TEntity> All(bool isReadonly, bool withDeleted);
 
-        IQueryable<TEntity> AllWithDeleted(Expression<Func<TEntity, bool>> search);
+        /// <summary>
+        /// This method returns a expression tree with all entities.
+        /// Depending on the <paramref name="isReadonly"/> parameter the entities will or will not be tracked.
+        /// Depending on the <paramref name="withDeleted"/> parameter all or only the not deleted entities will be returned.
+        /// Filtration is done by using the <paramref name="search"/> delegate.
+        /// </summary>
+        /// <param name="search">This is a filtration delegate.</param>
+        /// <param name="isReadonly">This flag decides whether the entities should be tracked or not.</param>
+        /// <param name="withDeleted">This flag decides whether all or only the not deleted entities should be returned.</param>
+        /// <returns>Returns an unmaterialized filtered query with the entities.</returns>
+        IQueryable<TEntity> All(Expression<Func<TEntity, bool>> search, bool isReadonly, bool withDeleted);
 
-        IQueryable<TEntity> AllWithDeletedAsNoTracking();
-
-        IQueryable<TEntity> AllWithDeletedAsNoTracking(Expression<Func<TEntity, bool>> search);
-
+        /// <summary>
+        /// This method deletes the entity from the database.
+        /// </summary>
+        /// <param name="entity">The entity to be deleted.</param>
         void HardDelete(TEntity entity);
 
+        /// <summary>
+        /// This method makes the IsDeleted flag of the entity to false and DeletedOn to null.
+        /// </summary>
+        /// <param name="entity">The entity to be undeleted.</param>
         void Undelete(TEntity entity);
     }
 }
--- src/Data/Roomed.Data/Repositories/EfDeletableEntityRepository.cs
+++ src/Data/Roomed.Data/Repositories/EfDeletableEntityRepository.cs
@@ -4,37 +4,78 @@
     using System.Linq;
     using System.Linq.Expressions;
 
-    using Microsoft.EntityFrameworkCore;
-
     using Roomed.Data.Common.Models;
     using Roomed.Data.Common.Repositories;
 
+    /// <summary>
+    /// An implementation of the <see cref="IDeletableEntityRepository{TEntity, TKey}"/> interface for the Entity Framework Core ORM.
+    /// </summary>
+    /// <typeparam name="TEntity">This is the entity that represents the table in the database.</typeparam>
+    /// <typeparam name="TKey">This is the type of the primary key of the entity.</typeparam>
     public class EfDeletableEntityRepository<TEntity, TKey> : EfRepository<TEntity, TKey>, IDeletableEntityRepository<TEntity, TKey>
         where TEntity : BaseDeletableModel<TKey>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EfDeletableEntityRepository{TEntity, TKey}"/> class.
+        /// </summary>
+        /// <param name="context">The ef core db context.</param>
+        /// <exception cref="ArgumentNullException">Throws when the db context is null.</exception>
         public EfDeletableEntityRepository(ApplicationDbContext context)
             : base(context)
         {
         }
 
-        public override IQueryable<TEntity> All() => base.All().Where(x => !x.IsDeleted);
 
-        public override IQueryable<TEntity> All(Expression<Func<TEntity, bool>> search) => this.All().Where(search);
+        /// <inheritdoc/>
+        public IQueryable<TEntity> All(bool isReadonly = false, bool withDeleted = false)
+        {
+            var query = this.All(isReadonly);
 
-        public override IQueryable<TEntity> AllAsNoTracking() => base.AllAsNoTracking().Where(x => !x.IsDeleted);
+            if (!withDeleted)
+            {
+                query.Where(e => !e.IsDeleted);
+            }
 
-        public override IQueryable<TEntity> AllAsNoTracking(Expression<Func<TEntity, bool>> search) => this.AllAsNoTracking().Where(search);
+            return query;
+        }
 
-        public IQueryable<TEntity> AllWithDeleted() => base.All().IgnoreQueryFilters();
+        /// <inheritdoc/>
+        public IQueryable<TEntity> All(Expression<Func<TEntity, bool>> search, bool isReadonly = false, bool withDeleted = false)
+        {
+            var query = this.All(search, isReadonly);
 
-        public IQueryable<TEntity> AllWithDeleted(Expression<Func<TEntity, bool>> search) => this.AllWithDeleted().Where(search);
+            if (!withDeleted)
+            {
+                query.Where(e => !e.IsDeleted);
+            }
 
-        public IQueryable<TEntity> AllWithDeletedAsNoTracking() => base.AllAsNoTracking().IgnoreQueryFilters();
+            return query;
+        }
 
-        public IQueryable<TEntity> AllWithDeletedAsNoTracking(Expression<Func<TEntity, bool>> search) => this.AllWithDeletedAsNoTracking().Where(search);
+        /// <summary>
+        /// This method returns a expression tree with all entities.
+        /// Depending on the <paramref name="isReadonly"/> parameter the entities will or will not be tracked.
+        /// Only entities that are not marked as deleted will be returned.
+        /// </summary>
+        /// <param name="isReadonly">This flag decides whether the entities should be tracked or not.</param>
+        /// <returns>Returns an unmaterialized query with all entities.</returns>
+        public override IQueryable<TEntity> All(bool isReadonly = false) => base.All(isReadonly).Where(x => !x.IsDeleted);
 
+        /// <summary>
+        /// This method returns a expression tree with all entities.
+        /// Depending on the <paramref name="isReadonly"/> parameter the entities will or will not be tracked.
+        /// Filtration is done by using the <paramref name="search"/> delegate.
+        /// Only entities that are not marked as deleted will be returned.
+        /// </summary>
+        /// <param name="search">This is a filtration delegate.</param>
+        /// <param name="isReadonly">This flag decides whether the entities should be tracked or not.</param>
+        /// <returns>Returns an unmaterialized filtered query with all entities.</returns>
+        public override IQueryable<TEntity> All(Expression<Func<TEntity, bool>> search, bool isReadonly = false) => this.All(isReadonly).Where(search);
+
+        /// <inheritdoc/>
         public void HardDelete(TEntity entity) => base.Delete(entity);
 
+        /// <inheritdoc/>
         public void Undelete(TEntity entity)
         {
             entity.IsDeleted = false;
@@ -42,6 +83,10 @@
             this.Update(entity);
         }
 
+        /// <summary>
+        /// This method sets the entity's IsDelted flag to true and DeletedOn to <see cref="DateTime.UtcNow"/>.
+        /// </summary>
+        /// <param name="entity">The entity to be deleted.</param>
         public override void Delete(TEntity entity)
         {
             entity.IsDeleted = true;
