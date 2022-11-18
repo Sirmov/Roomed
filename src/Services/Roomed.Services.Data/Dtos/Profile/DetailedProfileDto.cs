8234d987f6420b63d794e99d7d5a42a9a8d54fc4
Nikola Sirmov <sirmov0213@gmail.com>
2022-11-18 13:03:13 +0200

    Create DetailedProfileDto.cs

---
+++ src/Services/Roomed.Services.Data/Dtos/Profile/DetailedProfileDto.cs
@@ -0,0 +1,42 @@
+﻿namespace Roomed.Services.Data.Dtos.Profile
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Roomed.Data.Models.Enums;
+    using Roomed.Services.Mapping;
+
+    using static Roomed.Common.DataConstants.Profile;
+
+    public class DetailedProfileDto : IMapFrom<Roomed.Data.Models.Profile>
+    {
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(FirstNameMaxLength, MinimumLength = FirstNameMinLength)]
+        public string FirstName { get; set; } = null!;
+
+        [StringLength(MiddleNameMaxLenght, MinimumLength = MiddleNameMinLenght)]
+        public string? MiddleName { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(LastNameMaxLength, MinimumLength = LastNameMinLength)]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateOnly Birthdate { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(Gender))]
+        public Gender Gender { get; set; }
+
+        [Required]
+        [StringLength(NationalityMaxLength, MinimumLength = NationalityMinLength)]
+        public string Nationality { get; set; }
+
+        [Required]
+        [StringLength(NationalityCodeMaxLength, MinimumLength = NationalityCodeMinLength)]
+        public string NationalityCode { get; set; }
+
+        [StringLength(AddressMaxLength, MinimumLength = AddressMinLength)]
+        public string? Address { get; set; }
+    }
+}
