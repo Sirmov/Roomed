8b5a3bd1f289a3ee522c6f6c5494aa79329e7562
Nikola Sirmov <sirmov0213@gmail.com>
2022-11-18 13:03:10 +0200

    Create DetailedProfileInputModel.cs

---
+++ src/Web/Roomed.Web.ViewModels/Profile/DetailedProfileInputModel.cs
@@ -0,0 +1,44 @@
+﻿namespace Roomed.Web.ViewModels.Profile
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Roomed.Data.Models.Enums;
+    using Roomed.Services.Data.Dtos.Profile;
+    using Roomed.Services.Mapping;
+
+    using static Roomed.Common.DataConstants.Profile;
+
+    public class DetailedProfileInputModel : IMapTo<DetailedProfileDto>
+    {
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(FirstNameMaxLength, MinimumLength = FirstNameMinLength)]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; } = null!;
+
+        [StringLength(MiddleNameMaxLenght, MinimumLength = MiddleNameMinLenght)]
+        [Display(Name = "Middle name")]
+        public string? MiddleName { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(LastNameMaxLength, MinimumLength = LastNameMinLength)]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateOnly Birthdate { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(Gender))]
+        public Gender Gender { get; set; }
+
+        [Required]
+        public string Nationality { get; set; }
+
+        [Required]
+        public string NationalityCode { get; set; }
+
+        [StringLength(AddressMaxLength, MinimumLength = AddressMinLength)]
+        public string? Address { get; set; }
+    }
+}
