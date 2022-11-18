9adf2b02653eefcce8c4407b1cda4a73edb10d0c
Nikola Sirmov <sirmov0213@gmail.com>
2022-11-18 13:03:02 +0200

    Create ProfileInputModel.cs

---
+++ src/Web/Roomed.Web.ViewModels/Profile/ProfileInputModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Roomed.Web.ViewModels.Profile
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Roomed.Data.Models.Enums;
+
+    using static Roomed.Common.DataConstants.Profile;
+
+    public class ProfileInputModel
+    {
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(FirstNameMaxLength, MinimumLength = FirstNameMinLength)]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; } = null!;
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(LastNameMaxLength, MinimumLength = LastNameMinLength)]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; } = null!;
+    }
+}
