f2bb3e7beae1c0297e94275491cad7a51dd9f455
Nikola Sirmov <sirmov0213@gmail.com>
2022-11-20 18:41:06 +0200

    Added sanitize attributes

--- src/Web/Roomed.Web.ViewModels/Profile/DetailedProfileInputModel.cs
+++ src/Web/Roomed.Web.ViewModels/Profile/DetailedProfileInputModel.cs
@@ -2,6 +2,7 @@
 {
     using System.ComponentModel.DataAnnotations;
 
+    using Roomed.Common.Attribues;
     using Roomed.Data.Models.Enums;
     using Roomed.Services.Data.Dtos.Profile;
     using Roomed.Services.Mapping;
@@ -13,15 +14,18 @@
         [Required(AllowEmptyStrings = false)]
         [StringLength(FirstNameMaxLength, MinimumLength = FirstNameMinLength)]
         [Display(Name = "First name")]
+        [Sanitize]
         public string FirstName { get; set; } = null!;
 
         [StringLength(MiddleNameMaxLenght, MinimumLength = MiddleNameMinLenght)]
         [Display(Name = "Middle name")]
+        [Sanitize]
         public string? MiddleName { get; set; }
 
         [Required(AllowEmptyStrings = false)]
         [StringLength(LastNameMaxLength, MinimumLength = LastNameMinLength)]
         [Display(Name = "Last name")]
+        [Sanitize]
         public string LastName { get; set; } = null!;
 
         [Required]
@@ -33,12 +37,15 @@
         public Gender Gender { get; set; }
 
         [Required]
+        [Sanitize]
         public string Nationality { get; set; }
 
         [Required]
+        [Sanitize]
         public string NationalityCode { get; set; }
 
         [StringLength(AddressMaxLength, MinimumLength = AddressMinLength)]
+        [Sanitize]
         public string? Address { get; set; }
     }
 }
