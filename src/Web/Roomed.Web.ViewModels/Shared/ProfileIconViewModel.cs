adc25a2da89cad99ba9a68d051efaea234e081b4
Nikola Sirmov <sirmov0213@gmail.com>
2022-11-21 11:18:05 +0200

    Create ProfileIconViewModel.cs

---
+++ src/Web/Roomed.Web.ViewModels/Shared/ProfileIconViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Roomed.Web.ViewModels.Shared
+{
+    using Roomed.Data.Models.Enums;
+
+    public class ProfileIconViewModel
+    {
+        public Gender? Gender { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+    }
+}
