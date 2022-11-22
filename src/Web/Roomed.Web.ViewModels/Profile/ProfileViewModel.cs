d5791722eb59a1096f680458d3b70b5c0df8c2ed
Nikola Sirmov <sirmov0213@gmail.com>
2022-11-22 10:42:34 +0200

    Create ProfileViewModel.cs

---
+++ src/Web/Roomed.Web.ViewModels/Profile/ProfileViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Roomed.Web.ViewModels.Profile
+{
+    using AutoMapper.Configuration.Annotations;
+    using Roomed.Services.Mapping;
+
+    public class ProfileViewModel : IMapFrom<DetailedProfileViewModel>
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string? MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Ignore]
+        public string FullName
+        {
+            get => $"{this.FirstName} {(this.MiddleName == null ? string.Empty : $"{this.MiddleName} ")}{this.LastName}";
+        }
+    }
+}
