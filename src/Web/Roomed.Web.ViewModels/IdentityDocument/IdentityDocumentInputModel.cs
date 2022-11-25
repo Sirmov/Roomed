312bd6f963e4f976b702a803be973baf3c22d962
Nikola Sirmov <sirmov0213@gmail.com>
2022-11-25 12:15:10 +0200

    Add sanitize attribute

--- src/Web/Roomed.Web.ViewModels/IdentityDocument/IdentityDocumentInputModel.cs
+++ src/Web/Roomed.Web.ViewModels/IdentityDocument/IdentityDocumentInputModel.cs
@@ -1,14 +1,19 @@
 ﻿namespace Roomed.Web.ViewModels.IdentityDocument
 {
     using System.ComponentModel.DataAnnotations;
-    using Microsoft.AspNetCore.Mvc.Rendering;
+
+    using Roomed.Common.Attribues;
     using Roomed.Common.ValidationAttributes;
     using Roomed.Data.Models.Enums;
+    using Roomed.Services.Data.Dtos.IdentityDocument;
+    using Roomed.Services.Mapping;
 
     using static Roomed.Common.DataConstants.IdentityDocument;
 
-    public class IdentityDocumentInputModel
+    public class IdentityDocumentInputModel : IMapFrom<IdentityDocumentDto>, IMapTo<IdentityDocumentDto>
     {
+        public Guid? Id { get; set; }
+
         [Required(AllowEmptyStrings = false)]
         public Guid OwnerId { get; set; }
 
@@ -19,48 +24,55 @@
         [Required(AllowEmptyStrings = false)]
         [StringLength(NameMaxLength, MinimumLength = NameMinLength)]
         [Display(Name = "Name in document")]
+        [Sanitize]
         public string NameInDocument { get; set; }
 
         [Required(AllowEmptyStrings = false)]
         [StringLength(NumberMaxLength, MinimumLength = NumberMinLength)]
-        [Display(Name = "Document Number")]
+        [Display(Name = "Document number")]
+        [Sanitize]
         public string DocumentNumber { get; set; }
 
         [StringLength(PersonalNumberMaxLength, MinimumLength = PersonalNumberMinLength)]
         [Display(Name = "Personal number")]
+        [Sanitize]
         public string? PersonalNumber { get; set; }
 
+        [Required(AllowEmptyStrings = false)]
         [StringLength(CountryMaxLength, MinimumLength = CountryMinLength)]
+        [Sanitize]
         public string Country { get; set; }
 
         [Required]
         [DataType(DataType.Date)]
-        public DateOnly Birthdate { get; set; }
+        public DateOnly? Birthdate { get; set; }
 
         [Required]
         [StringLength(PlaceOfBirthMaxLength, MinimumLength = PlaceOfBirthMinLength)]
         [Display(Name = "Place of birth")]
+        [Sanitize]
         public string PlaceOfBirth { get; set; }
 
         [Required]
-        [StringLength(NationalityMaxLength, MinimumLength = NationalityMinLength)]
+        [Sanitize]
         public string Nationality { get; set; }
 
         [Required]
         [BeforeDate(nameof(ValidUntil))]
         [DataType(DataType.Date)]
         [Display(Name = "Valid from")]
-        public DateOnly ValidFrom { get; set; }
+        public DateOnly? ValidFrom { get; set; }
 
         [Required]
         [AfterDate(nameof(ValidFrom))]
         [DataType(DataType.Date)]
         [Display(Name = "Valid until")]
-        public DateOnly ValidUntil { get; set; }
+        public DateOnly? ValidUntil { get; set; }
 
         [Required(AllowEmptyStrings = false)]
         [StringLength(IssuedByMaxLength, MinimumLength = IssuedByMinLength)]
         [Display(Name = "Issued by")]
+        [Sanitize]
         public string IssuedBy { get; set; }
     }
 }
