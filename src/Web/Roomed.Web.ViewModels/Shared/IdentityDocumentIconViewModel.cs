88db8abab794264c8fbe418d44d80082c0698aea
Nikola Sirmov <sirmov0213@gmail.com>
2022-11-25 12:12:11 +0200

    Identity document icon partial

---
+++ src/Web/Roomed.Web.ViewModels/Shared/IdentityDocumentIconViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Roomed.Web.ViewModels.Shared
+{
+    using Roomed.Data.Models.Enums;
+
+    public class IdentityDocumentIconViewModel
+    {
+        public IdentityDocumentType? Type { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+    }
+}
