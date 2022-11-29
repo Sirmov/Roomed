30def6effef009549e97bc17c39a3a246f15c4de
Nikola Sirmov <sirmov0213@gmail.com>
2022-11-29 10:21:01 +0200

    Added validation attributes
    q

--- src/Services/Roomed.Services.Data/Dtos/Reservation/ReservationDto.cs
+++ src/Services/Roomed.Services.Data/Dtos/Reservation/ReservationDto.cs
@@ -1,24 +1,49 @@
 ﻿namespace Roomed.Services.Data.Dtos.Reservation
 {
+    using System.ComponentModel.DataAnnotations;
+
+    using Roomed.Common.ValidationAttributes;
     using Roomed.Data.Models.Enums;
     using Roomed.Services.Data.Dtos.Profile;
     using Roomed.Services.Data.Dtos.ReservationDay;
     using Roomed.Services.Data.Dtos.RoomType;
     using Roomed.Services.Mapping;
 
+    using static Roomed.Common.DataConstants.Reservation;
+
     public class ReservationDto : IMapFrom<Roomed.Data.Models.Reservation>
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
 
+        [Required]
         public Guid ReservationHolderId { get; set; }
 
+        [Required]
+        [BeforeDate(nameof(DepartureDate))]
         public DateOnly ArrivalDate { get; set; }
 
+        [Required]
+        [AfterDate(nameof(ArrivalDate))]
         public DateOnly DepartureDate { get; set; }
 
+        [Required]
+        [EnumDataType(typeof(ReservationStatus))]
         public ReservationStatus Status { get; set; }
 
+        [Required]
         public int RoomTypeId { get; set; }
+
+        [Required]
+        [Range(0, AdultsMaxCount)]
+        public int Adults { get; set; }
+
+        [Required]
+        [Range(0, TeenagersMaxCount)]
+        public int Teenagers { get; set; }
+
+        [Required]
+        [Range(0, ChildrenMaxCount)]
+        public int Children { get; set; }
 
         public DetailedProfileDto ReservationHolder { get; set; } = null!;
 
