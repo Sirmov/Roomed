e3be6fdb48a6634ad448f9b1940778bb8327afcc
Nikola Sirmov <sirmov0213@gmail.com>
2022-11-29 12:35:50 +0200

    Reservation days service interface

---
+++ src/Services/Roomed.Services.Data/Contracts/IReservationDaysService.cs
@@ -0,0 +1,12 @@
+﻿namespace Roomed.Services.Data.Contracts
+{
+    using Roomed.Services.Data.Common;
+    using Roomed.Services.Data.Dtos.ReservationDay;
+
+    public interface IReservationDaysService
+    {
+        public Task<ICollection<ReservationDayDto>> GetAllForDate(DateOnly date, QueryOptions<ReservationDayDto>? queryOptions);
+
+        public Task<ICollection<ReservationDayDto>> GetAllForPeriod(DateOnly startDate, DateOnly endDate, QueryOptions<ReservationDayDto>? queryOptions);
+    }
+}
