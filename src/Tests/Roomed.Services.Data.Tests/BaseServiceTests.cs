109421bef0b7d1632356762528f6b61405cb69f6
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-04 21:44:58 +0200

    BaseService not implemented tests

---
+++ src/Tests/Roomed.Services.Data.Tests/BaseServiceTests.cs
@@ -0,0 +1,111 @@
+﻿namespace Roomed.Services.Data.Tests
+{
+    [TestFixture]
+    public class BaseServiceTests
+    {
+        // GetAllAsync
+
+        [Test]
+        public async Task GetAllAsyncShouldReturnAllNotDeletedEntitiesWithTracking()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public async Task GetAllAsyncShouldReturnAllEntitiesWithDeletedWithoutTracking()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public async Task GetAllAsyncShouldReturnAllEntitiesInCorrectOrder()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public async Task GetAllAsyncShouldReturnAllEntitiesSkippingFirstNAmount()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public async Task GetAllAsyncShouldReturnAllEntitiesTakingFirstNAmount()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        // GetAsync
+
+        [Test]
+        public async Task GetAsyncShouldThrowWhenIdIsNull()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public async Task GetAsyncShouldReturnEntityDtoWithGivenId()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        // ValidateDto
+
+        [Test]
+        public async Task ValidateDtoShouldReturnFalse()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public async Task ValidateDtoShouldReturnTrue()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
