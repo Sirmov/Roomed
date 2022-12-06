24a2d303ad74f86ee237f66a43cee45f9af8dc1e
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-06 20:33:38 +0200

    Test class to expose protected method

---
+++ src/Tests/Roomed.Services.Data.Tests/TestClasses/BaseServiceTest.cs
@@ -0,0 +1,22 @@
+﻿namespace Roomed.Services.Data.Tests.TestClasses
+{
+    using AutoMapper;
+
+    using Roomed.Data.Common.Models;
+    using Roomed.Data.Common.Repositories;
+    using Roomed.Services.Data.Common;
+
+    public class BaseServiceTest<TEntity, TKey> : BaseService<TEntity, TKey>
+        where TEntity : BaseDeletableModel<TKey>
+    {
+        public BaseServiceTest(IDeletableEntityRepository<TEntity, TKey> entityRepository, IMapper mapper)
+            : base(entityRepository, mapper)
+        {
+        }
+
+        public bool ValidateDto<TDto>(TDto dto)
+        {
+            return base.ValidateDto(dto);
+        }
+    }
+}
