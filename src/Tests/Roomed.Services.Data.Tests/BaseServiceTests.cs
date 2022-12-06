76cf65bdc8b53f1a3f60fb732a958573abf3db80
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-06 20:33:51 +0200

    Base service tests

--- src/Tests/Roomed.Services.Data.Tests/BaseServiceTests.cs
+++ src/Tests/Roomed.Services.Data.Tests/BaseServiceTests.cs
@@ -1,111 +1,220 @@
 ﻿namespace Roomed.Services.Data.Tests
 {
+    using System.Linq;
+    using System.Reflection;
+
+    using AutoMapper;
+    using Microsoft.EntityFrameworkCore;
+
+    using Roomed.Data.Common.Repositories;
+    using Roomed.Data.Models;
+    using Roomed.Services.Data.Common;
+    using Roomed.Services.Data.Dtos.ReservationNote;
+    using Roomed.Services.Data.Tests.TestClasses;
+
+    using static Roomed.Common.DataConstants.ReservationNote;
+
     [TestFixture]
     public class BaseServiceTests
     {
+        private IMapper mapper;
+        private IDeletableEntityRepository<ReservationNote, Guid> repository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.mapper = GetMapper();
+            this.repository = GetReservationNotesRepository();
+        }
+
         // GetAllAsync
 
         [Test]
-        public async Task GetAllAsyncShouldReturnAllNotDeletedEntitiesWithTracking()
+        public async Task GetAllAsyncShouldReturnAllNotDeletedEntities()
         {
             // Arrange
+            BaseService<ReservationNote, Guid> baseService = new(this.repository, this.mapper);
 
             // Act
+            var dtos = await baseService.GetAllAsync<ReservationNoteDto>();
 
             // Assert
-            Assert.Fail();
+            Assert.That(dtos.Count, Is.EqualTo(2), "Entities count is not correct.");
+            Assert.That(dtos, Has.All.Matches<object>(x => x is ReservationNoteDto), "Some entities are not of the correct type.");
         }
 
         [Test]
-        public async Task GetAllAsyncShouldReturnAllEntitiesWithDeletedWithoutTracking()
+        public async Task GetAllAsyncShouldReturnAllEntitiesWithDeleted()
         {
             // Arrange
+            BaseService<ReservationNote, Guid> baseService = new(this.repository, this.mapper);
 
             // Act
+            var dtos = await baseService.GetAllAsync<ReservationNoteDto>(new()
+            {
+                WithDeleted = true
+            });
 
             // Assert
-            Assert.Fail();
+            Assert.That(dtos.Count, Is.EqualTo(3), "Entities count is not correct.");
+            Assert.That(dtos, Has.All.Matches<object>(x => x is ReservationNoteDto), "Some entities are not of the correct type.");
         }
 
         [Test]
         public async Task GetAllAsyncShouldReturnAllEntitiesInCorrectOrder()
         {
             // Arrange
+            BaseService<ReservationNote, Guid> baseService = new(this.repository, this.mapper);
 
             // Act
+            var dtos = await baseService.GetAllAsync<ReservationNoteDto>(new()
+            {
+                OrderOptions = new()
+                {
+                    new OrderOption<ReservationNoteDto>(e => e.Body, OrderByOrder.Ascending),
+                },
+                WithDeleted = true
+            });
 
             // Assert
-            Assert.Fail();
+            var reservationNotes = await this.repository.All(true, true).ToListAsync();
+            Assert.That(dtos.Count, Is.EqualTo(3), "Entities count is not correct.");
+            Assert.That(dtos, Has.All.Matches<object>(x => x is ReservationNoteDto), "Some entities are not of the correct type.");
+            Assert.That(dtos.First().Body, Is.EqualTo(reservationNotes.First().Body), "Entities are not in correct order.");
         }
 
         [Test]
-        public async Task GetAllAsyncShouldReturnAllEntitiesSkippingFirstNAmount()
+        [TestCase(1)]
+        [TestCase(2)]
+        public async Task GetAllAsyncShouldReturnAllEntitiesSkippingFirstNAmount(int n)
         {
             // Arrange
+            BaseService<ReservationNote, Guid> baseService = new(this.repository, this.mapper);
 
             // Act
+            var dtos = await baseService.GetAllAsync<ReservationNoteDto>(new()
+            {
+                Skip = n,
+                WithDeleted = true
+            });
 
             // Assert
-            Assert.Fail();
+            var reservationNotes = await this.repository.All(true, true).ToListAsync();
+            Assert.That(dtos.Count, Is.EqualTo(reservationNotes.Count - n), "Entities count is not correct.");
+            Assert.That(dtos, Has.All.Matches<object>(x => x is ReservationNoteDto), "Some entities are not of the correct type.");
+            Assert.That(dtos.First().Body,
+                Is.EqualTo(reservationNotes[reservationNotes.Count - n - 1].Body), "Entities are not in correct order.");
         }
 
         [Test]
-        public async Task GetAllAsyncShouldReturnAllEntitiesTakingFirstNAmount()
+        [TestCase(1)]
+        [TestCase(2)]
+        public async Task GetAllAsyncShouldReturnAllEntitiesTakingFirstNAmount(int n)
         {
             // Arrange
+            BaseService<ReservationNote, Guid> baseService = new(this.repository, this.mapper);
 
             // Act
+            var dtos = await baseService.GetAllAsync<ReservationNoteDto>(new()
+            {
+                Take = n,
+                WithDeleted = true
+            });
 
             // Assert
-            Assert.Fail();
+            var reservationNotes = await this.repository.All(true, true).ToListAsync();
+            Assert.That(dtos.Count, Is.EqualTo(reservationNotes.Count - n), "Entities count is not correct.");
+            Assert.That(dtos, Has.All.Matches<object>(x => x is ReservationNoteDto), "Some entities are not of the correct type.");
+            Assert.That(dtos.Last().Body,
+                Is.EqualTo(reservationNotes[n - 1].Body), "Entities are not in correct order.");
         }
 
         // GetAsync
 
         [Test]
-        public async Task GetAsyncShouldThrowWhenIdIsNull()
+        [TestCase("8d162afd-bcb2-4ccb-81c0-96965f7177d0")]
+        public void GetAsyncShouldThrowWhenEntityDoesNotExist(string id)
         {
             // Arrange
+            BaseService<ReservationNote, Guid> baseService = new(this.repository, this.mapper);
+            Guid guid = Guid.Parse(id);
 
             // Act
+            Func<Task> code = async () => await baseService.GetAsync<ReservationNoteDto>(guid);
 
             // Assert
-            Assert.Fail();
+            Assert.ThrowsAsync<InvalidOperationException>(async () => await code(), "Should throw when entitiy is not found.");
         }
 
         [Test]
-        public async Task GetAsyncShouldReturnEntityDtoWithGivenId()
+        [TestCase("bb2f7b6c-d8d9-4e2c-b14b-3bd98e18ad86")]
+        public async Task GetAsyncShouldReturnEntityDtoWithGivenId(string id)
         {
             // Arrange
+            BaseService<ReservationNote, Guid> baseService = new(this.repository, this.mapper);
+            Guid guid = Guid.Parse(id);
 
             // Act
+            var dto = await baseService.GetAsync<ReservationNoteDto>(guid);
 
             // Assert
-            Assert.Fail();
+            var reservationNotes = await this.repository.All(true, true).ToListAsync();
+            var reservationNote = reservationNotes.First(rn => rn.Id == guid);
+            Assert.That(reservationNote, Is.Not.Null, "Entity does not exist.");
+            Assert.IsTrue(dto is ReservationNoteDto, "Entity is not of correct type.");
+            Assert.That(dto.Body, Is.EqualTo(reservationNote.Body), "Entities do not match.");
         }
 
         // ValidateDto
 
         [Test]
-        public async Task ValidateDtoShouldReturnFalse()
+        public void ValidateDtoShouldReturnFalse()
         {
             // Arrange
+            BaseServiceTest<ReservationNote, Guid> baseService = new(this.repository, this.mapper);
+
+            var shortDto = new ReservationNoteDto()
+            {
+                Body = new string('*', BodyMinLength - 1),
+            };
+
+            var longDto = new ReservationNoteDto()
+            {
+                Body = new string('*', BodyMaxLength + 1),
+            };
 
             // Act
+            var shortResult = baseService.ValidateDto(shortDto);
+            var longResult = baseService.ValidateDto(longDto);
 
             // Assert
-            Assert.Fail();
+            Assert.IsFalse(shortResult, "Validation result should be false.");
+            Assert.IsFalse(longResult, "Validation result should be false.");
         }
 
         [Test]
-        public async Task ValidateDtoShouldReturnTrue()
+        public void ValidateDtoShouldReturnTrue()
         {
             // Arrange
+            BaseServiceTest<ReservationNote, Guid> baseService = new(this.repository, this.mapper);
+
+            var shortDto = new ReservationNoteDto()
+            {
+                Body = new string('*', BodyMinLength),
+            };
+
+            var longDto = new ReservationNoteDto()
+            {
+                Body = new string('*', BodyMaxLength),
+            };
 
             // Act
+            var shortResult = baseService.ValidateDto(shortDto);
+            var longResult = baseService.ValidateDto(longDto);
 
             // Assert
-            Assert.Fail();
+            Assert.IsTrue(shortResult, "Validation result should be true.");
+            Assert.IsTrue(longResult, "Validation result should be true.");
         }
     }
 }
