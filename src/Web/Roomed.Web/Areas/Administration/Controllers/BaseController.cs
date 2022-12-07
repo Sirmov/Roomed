47d9c7fe7f2ae85a80e4e8a0ddc06792fd7aa457
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-07 12:36:01 +0200

    Administration area

---
+++ src/Web/Roomed.Web/Areas/Administration/Controllers/BaseController.cs
@@ -0,0 +1,54 @@
+﻿namespace Roomed.Web.Areas.Administration.Controllers
+{
+    using System.Reflection;
+
+    using Ganss.Xss;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using Roomed.Common.Attribues;
+
+    using static Roomed.Common.AreasControllersActionsConstants;
+
+    /// <summary>
+    /// The base controller is a base class for all controllers in the administration area.
+    /// It inherits the default ASP MVC <see cref="Controller"/>.
+    /// </summary>
+    [Area(Areas.Administration)]
+    [Authorize(Policy = "Administration")]
+    public class BaseController : Controller
+    {
+        protected IHtmlSanitizer sanitizer;
+
+        public BaseController(IHtmlSanitizer sanitizer)
+        {
+            this.sanitizer = sanitizer;
+        }
+
+        protected void SanitizeModel<TModel>(TModel model)
+        {
+            ArgumentNullException.ThrowIfNull(model, nameof(model));
+
+            var type = model.GetType();
+
+            var flags = BindingFlags.Public | BindingFlags.Instance;
+
+            var properties = type.GetProperties(flags);
+
+            foreach (var property in properties)
+            {
+                bool shouldBeSanitized = property.IsDefined(typeof(SanitizeAttribute), false);
+
+                if (property.PropertyType == typeof(string) && shouldBeSanitized)
+                {
+                    string? value = property.GetValue(model) as string;
+
+                    if (value != null)
+                    {
+                        property.SetValue(model, this.sanitizer.Sanitize(value));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Web/Roomed.Web/Areas/Administration/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿namespace Roomed.Web.Areas.Administration.Controllers
+{
+    using Ganss.Xss;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : BaseController
+    {
+        public HomeController(IHtmlSanitizer sanitizer)
+            : base(sanitizer)
+        {
+        }
+
+        /// <summary>
+        /// This method returns the administration home page.
+        /// </summary>
+        /// <returns>Returns <see cref="IActionResult"/>.</returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// This method returns the not implemented page.
+        /// </summary>
+        /// <returns>Returns <see cref="IActionResult"/>.</returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult NotImplemented()
+        {
+            return View();
+        }
+    }
+}
