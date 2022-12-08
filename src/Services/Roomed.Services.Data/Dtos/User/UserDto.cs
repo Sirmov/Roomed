38c75e2aa8c8faa845dbd8c320b27ba170caccbf
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-08 12:26:28 +0200

    User models

---
+++ src/Services/Roomed.Services.Data/Dtos/User/UserDto.cs
@@ -0,0 +1,25 @@
+﻿namespace Roomed.Services.Data.Dtos.User
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Roomed.Common.DataConstants.ApplicationUser;
+
+    public class UserDto
+    {
+        public Guid? Id { get; set; }
+
+        [EmailAddress]
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(EmailMaxLength, MinimumLength = EmailMinLength)]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(UserNameMaxLength, MinimumLength = UserNameMinLength)]
+        public string UserName { get; set; }
+
+        [StringLength(PasswordMaxLength, MinimumLength = PasswordMinLength)]
+        public string? Password { get; set; }
+
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
---
+++ src/Web/Roomed.Web.ViewModels/User/UserInputModel.cs
@@ -0,0 +1,36 @@
+﻿namespace Roomed.Web.ViewModels.User
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Roomed.Common.Attribues;
+    using Roomed.Data.Models;
+    using Roomed.Services.Data.Dtos.User;
+    using Roomed.Services.Mapping;
+
+    using static Roomed.Common.DataConstants.ApplicationUser;
+
+    public class UserInputModel : IMapFrom<ApplicationUser>, IMapTo<UserDto>
+    {
+        public Guid? Id { get; set; }
+
+        [Sanitize]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(EmailMaxLength, MinimumLength = EmailMinLength)]
+        public string Email { get; set; }
+
+        [Sanitize]
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Username")]
+        [StringLength(UserNameMaxLength, MinimumLength = UserNameMinLength)]
+        public string UserName { get; set; }
+
+        [Sanitize]
+        [DataType(DataType.Password)]
+        [StringLength(PasswordMaxLength, MinimumLength = PasswordMinLength)]
+        public string? Password { get; set; }
+
+        public IEnumerable<string> Roles { get; set; } = new List<string>();
+    }
+}
