211306f50f39034f00b4f5eb74845db46647948b
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-08 21:19:35 +0200

    User view model

---
+++ src/Web/Roomed.Web.ViewModels/User/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Roomed.Web.ViewModels.User
+{
+    using Roomed.Data.Models;
+    using Roomed.Services.Data.Dtos.User;
+    using Roomed.Services.Mapping;
+
+    public class UserViewModel : IMapFrom<ApplicationUser>, IMapTo<UserDto>
+    {
+        public Guid Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+
+        public IEnumerable<string> Roles { get; set; } = new List<string>();
+    }
+}
