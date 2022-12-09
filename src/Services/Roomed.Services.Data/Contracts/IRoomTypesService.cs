8e68dda1a7761f4d7704d9f0d70ca43e5a181838
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-09 10:51:27 +0200

    Room types service

---
+++ src/Services/Roomed.Services.Data/Contracts/IRoomTypesService.cs
@@ -0,0 +1,34 @@
+﻿namespace Roomed.Services.Data.Contracts
+{
+    using Roomed.Services.Data.Common;
+    using Roomed.Services.Data.Dtos.RoomType;
+
+    /// <summary>
+    /// This interface is used to state and document the room types data service functionality.
+    /// </summary>
+    public interface IRoomTypesService
+    {
+        /// <summary>
+        /// This method asynchronously returns a collection of all room types.
+        /// </summary>
+        /// <param name="queryOptions">The query options.</param>
+        /// <returns>Returns a collection of <see cref="RoomTypeDto"/> objects.</returns>
+        public Task<ICollection<RoomTypeDto>> GetAllAsync(QueryOptions<RoomTypeDto>? queryOptions = null);
+
+        /// <summary>
+        /// This method asynchronously returns the room type with the corresponding id.
+        /// </summary>
+        /// <param name="id">The id of the profile.</param>
+        /// <param name="queryOptions">The query options.</param>
+        /// <returns>Returns the <see cref="RoomTypeDto"/> with the given id.</returns>
+        public Task<RoomTypeDto> GetAsync(int id, QueryOptions<RoomTypeDto>? queryOptions = null);
+
+        /// <summary>
+        /// This method asynchronously checks if a room type exists.
+        /// </summary>
+        /// <param name="id">The id of the room type.</param>
+        /// <param name="queryOptions">The query options.</param>
+        /// <returns>Returns a <see cref="Task"/> of <see cref="bool"/>.</returns>
+        public Task<bool> ExistsAsync(int id, QueryOptions<RoomTypeDto>? queryOptions = null);
+    }
+}
---
+++ src/Services/Roomed.Services.Data/RoomTypesService.cs
@@ -0,0 +1,55 @@
+﻿namespace Roomed.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+
+    using Roomed.Data.Common.Repositories;
+    using Roomed.Data.Models;
+    using Roomed.Services.Data.Common;
+    using Roomed.Services.Data.Contracts;
+    using Roomed.Services.Data.Dtos.RoomType;
+
+    public class RoomTypesService : BaseService<RoomType, int>, IRoomTypesService
+    {
+        private readonly IDeletableEntityRepository<RoomType, int> roomTypesRepository;
+
+        public RoomTypesService(
+            IDeletableEntityRepository<RoomType, int> entityRepository,
+            IMapper mapper)
+            : base(entityRepository, mapper)
+        {
+            this.roomTypesRepository = entityRepository;
+        }
+
+        /// <inheritdoc/>
+        public async Task<ICollection<RoomTypeDto>> GetAllAsync(QueryOptions<RoomTypeDto>? queryOptions = null)
+        {
+            return await base.GetAllAsync(queryOptions ?? new ());
+        }
+
+        /// <inheritdoc/>
+        public async Task<RoomTypeDto> GetAsync(int id, QueryOptions<RoomTypeDto>? queryOptions = null)
+        {
+            return await base.GetAsync(id, queryOptions ?? new ());
+        }
+
+        /// <inheritdoc/>
+        public async Task<bool> ExistsAsync(int id, QueryOptions<RoomTypeDto>? queryOptions = null)
+        {
+            var result = true;
+
+            try
+            {
+                await this.roomTypesRepository.FindAsync(id);
+            }
+            catch (InvalidOperationException iox)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
