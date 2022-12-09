7cebf79a532cb42e5359ab4e140f12bc73a0e78e
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-09 10:51:47 +0200

    Added validation attributes

--- src/Services/Roomed.Services.Data/Dtos/RoomType/RoomTypeDto.cs
+++ src/Services/Roomed.Services.Data/Dtos/RoomType/RoomTypeDto.cs
@@ -1,11 +1,17 @@
 ﻿namespace Roomed.Services.Data.Dtos.RoomType
 {
+    using System.ComponentModel.DataAnnotations;
+
     using Roomed.Services.Mapping;
+
+    using static Roomed.Common.DataConstants.RoomType;
 
     public class RoomTypeDto : IMapFrom<Roomed.Data.Models.RoomType>
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
 
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength)]
         public string Name { get; set; } = null!;
     }
 }
