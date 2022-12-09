b26ad3570964b4fdcc85195038389ea3fd32f2db
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-09 10:54:09 +0200

    Create reservation functionality

--- src/Web/Roomed.Web/Controllers/ReservationsController.cs
+++ src/Web/Roomed.Web/Controllers/ReservationsController.cs
@@ -3,11 +3,16 @@
     using AutoMapper;
     using Ganss.Xss;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+
     using Roomed.Data.Models;
-    using Roomed.Services.Data.Common;
     using Roomed.Services.Data.Contracts;
-    using Roomed.Services.Data.Dtos.Reservation;
+    using Roomed.Web.ViewModels.Profile;
     using Roomed.Web.ViewModels.Reservation;
+    using Roomed.Web.ViewModels.RoomType;
+
+    using static Roomed.Common.AreasControllersActionsConstants;
 
     /// <summary>
     /// A MVC controller inheriting <see cref="BaseController"/>.
@@ -16,6 +21,8 @@
     public class ReservationsController : BaseController
     {
         private readonly IReservationsService reservationsService;
+        private readonly IProfilesService profilesService;
+        private readonly IRoomTypesService roomTypesService;
         private readonly IMapper mapper;
 
         /// <summary>
@@ -23,12 +30,22 @@
         /// Uses constructor injection to resolve dependencies.
         /// </summary>
         /// <param name="reservationsService">The implementation of <see cref="IReservationsService"/>.</param>
+        /// <param name="profilesService">The implementation of <see cref="IProfilesService"/>.</param>
+        /// <param name="roomTypesService">The implementation of <see cref="IRoomTypesService"/>.</param>
         /// <param name="mapper">The global auto mapper.</param>
-        public ReservationsController(IReservationsService reservationsService, IMapper mapper, IHtmlSanitizer sanitizer)
+        /// <param name="sanitizer">The global html sanitizer.</param>
+        public ReservationsController(
+            IReservationsService reservationsService,
+            IProfilesService profilesService,
+            IRoomTypesService roomTypesService,
+            IMapper mapper,
+            IHtmlSanitizer sanitizer)
             : base(sanitizer)
         {
             this.reservationsService = reservationsService;
             this.mapper = mapper;
+            this.profilesService = profilesService;
+            this.roomTypesService = roomTypesService;
         }
 
         /// <summary>
@@ -45,6 +62,76 @@
             var model = reservations.Select(r => mapper.Map<ReservationViewModel>(r));
 
             return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var model = new ReservationInputModel();
+            var profiles = await this.profilesService.GetAllAsync();
+            var roomTypes = (await this.roomTypesService.GetAllAsync())
+                .Select(rt => this.mapper.Map<RoomTypeViewModel>(rt));
+
+            ViewBag.Profiles = profiles.Select(p => this.mapper.Map<DetailedProfileViewModel>(p));
+            ViewBag.RoomTypesSelectList = new SelectList(
+                    roomTypes.Select(rt => new SelectListItem() { Value = rt.Id.ToString(), Text = rt.Name }),
+                    "Value",
+                    "Text");
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ReservationInputModel model)
+        {
+            await this.ValidateReservation(ModelState, model);
+
+            if (!ModelState.IsValid)
+            {
+                var profiles = await this.profilesService.GetAllAsync();
+                var roomTypes = (await this.roomTypesService.GetAllAsync())
+                    .Select(rt => this.mapper.Map<RoomTypeViewModel>(rt));
+
+                ViewBag.Profiles = profiles.Select(p => this.mapper.Map<DetailedProfileViewModel>(p));
+                ViewBag.RoomTypesSelectList = new SelectList(
+                        roomTypes.Select(rt => new SelectListItem() { Value = rt.Id.ToString(), Text = rt.Name }),
+                        "Value",
+                        "Text");
+
+                return View(model);
+            }
+
+            return RedirectToAction(Actions.ChooseRoom, Controllers.Reservations, new { model = model });
+        }
+
+        private async Task ValidateReservation(ModelStateDictionary modelState, ReservationInputModel model)
+        {
+            if (modelState.IsValid == false)
+            {
+                return;
+            }
+
+            if (!await this.profilesService.ExistsAsync(model.ReservationHolderId))
+            {
+                modelState.AddModelError(nameof(model.ReservationHolderId), "Guest profile does not exist.");
+            }
+
+            if (!await this.roomTypesService.ExistsAsync(model.RoomTypeId))
+            {
+                modelState.AddModelError(nameof(model.RoomTypeId), "Room type does not exist.");
+            }
+
+            var today = DateOnly.FromDateTime(DateTime.Now);
+
+            if (model.ArrivalDate < today)
+            {
+                ModelState.AddModelError(nameof(model.ArrivalDate), "Can not create a reservation from the past.");
+            }
+
+            if (model.Adults < 1)
+            {
+                ModelState.AddModelError(nameof(model.Adults), "There should be at least one adult in a room.");
+            }
         }
     }
 }
---
+++ src/Web/Roomed.Web/wwwroot/js/views/reservations/create.js
@@ -0,0 +1,22 @@
+﻿const allowedData = ['ReservationHolderId', 'ArrivalDate', 'DepartureDate', 'RoomTypeId', 'Adults', 'Teenagers', 'Children'];
+const storageKey = 'createReservation';
+const createProfileLinkElement = document.getElementById('create-profile-link');
+const reservationsForm = document.getElementById('reservation-form');
+
+if (sessionStorage.getItem(storageKey) !== null) {
+    let data = JSON.parse(sessionStorage.getItem(storageKey));
+    let inputs = document.querySelectorAll('input.form-control');
+
+    inputs.forEach(i => {
+        i.value = data[i.id];
+    });
+
+    sessionStorage.removeItem(storageKey);
+}
+
+createProfileLinkElement.addEventListener('click', (e) => {
+    const formData = new FormData(reservationsForm);
+    const formEntries = [...formData.entries()];
+    let = data = formEntries.reduce((a, [k, v]) => (allowedData.includes(k) ? Object.assign(a, { [k]: v }) : a), {});
+    sessionStorage.setItem(storageKey, JSON.stringify(data));
+});
