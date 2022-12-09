156c3027fe96f5d689fbcf39aadc19ca853128ec
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-09 16:47:05 +0200

    Reservation days service

--- src/Services/Roomed.Services.Data/Contracts/IReservationDaysService.cs
+++ src/Services/Roomed.Services.Data/Contracts/IReservationDaysService.cs
@@ -1,12 +1,52 @@
 ﻿namespace Roomed.Services.Data.Contracts
 {
+    using Roomed.Data.Models;
     using Roomed.Services.Data.Common;
     using Roomed.Services.Data.Dtos.ReservationDay;
 
+    /// <summary>
+    /// This interface is used to state and document the reservation days data service functionality.
+    /// </summary>
     public interface IReservationDaysService
     {
-        public Task<ICollection<ReservationDayDto>> GetAllForDate(DateOnly date, QueryOptions<ReservationDayDto>? queryOptions);
+        /// <summary>
+        /// This method returns all reservation days asynchronously.
+        /// </summary>
+        /// <param name="queryOptions">The query options.</param>
+        /// <returns>Returns a <see cref="Task"/> with a collection of all <see cref="ReservationDayDto"/>.</returns>
+        public Task<ICollection<ReservationDayDto>> GetAllAsync(QueryOptions<ReservationDayDto>? queryOptions = null );
 
-        public Task<ICollection<ReservationDayDto>> GetAllForPeriod(DateOnly startDate, DateOnly endDate, QueryOptions<ReservationDayDto>? queryOptions);
+        /// <summary>
+        /// This method returns the reservation day with the given id asynchronously.
+        /// </summary>
+        /// <param name="id">The id of the reservation day.</param>
+        /// <param name="queryOptions">The query options.</param>
+        /// <returns>Returns a <see cref="Task"/> of <see cref="ReservationDayDto"/>.</returns>
+        public Task<ReservationDayDto> GetAsync(Guid id, QueryOptions<ReservationDayDto>? queryOptions = null);
+
+        /// <summary>
+        /// This method return all reservation days in a given data.
+        /// </summary>
+        /// <param name="date">The date of the reservation days.</param>
+        /// <param name="queryOptions">The query options.</param>
+        /// <returns>Returns a <see cref="Task"/> with a collection of all <see cref="ReservationDayDto"/> on a given date.</returns>
+        public Task<ICollection<ReservationDayDto>> GetAllForDate(DateOnly date, QueryOptions<ReservationDayDto>? queryOptions = null);
+
+        /// <summary>
+        /// This method return all reservation days in a given period.
+        /// </summary>
+        /// <param name="startDate">The start of the period.</param>
+        /// <param name="endDate">The end of the period.</param>
+        /// <param name="queryOptions">The query options.</param>
+        /// <returns>Returns a <see cref="Task"/> with a collection of all <see cref="ReservationDayDto"/> on a given period.</returns>
+        public Task<ICollection<ReservationDayDto>> GetAllForPeriod(DateOnly startDate, DateOnly endDate, QueryOptions<ReservationDayDto>? queryOptions = null);
+
+        /// <summary>
+        /// This method creates all reservation days for a newly created reservation.
+        /// </summary>
+        /// <param name="reservation">The newly created reservation.</param>
+        /// <param name="roomId">The id of the room to be occupied.</param>
+        /// <returns>Returns a <see cref="Task"/>.</returns>
+        public Task CreateForReservationAsync(Reservation reservation, int roomId);
     }
 }
---
+++ src/Services/Roomed.Services.Data/ReservationDaysService.cs
@@ -0,0 +1,105 @@
+﻿namespace Roomed.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Microsoft.EntityFrameworkCore;
+
+    using Roomed.Data.Common.Repositories;
+    using Roomed.Data.Models;
+    using Roomed.Services.Data.Common;
+    using Roomed.Services.Data.Contracts;
+    using Roomed.Services.Data.Dtos.ReservationDay;
+
+    public class ReservationDaysService : BaseService<ReservationDay, Guid>, IReservationDaysService
+    {
+        private readonly IDeletableEntityRepository<ReservationDay, Guid> reservationDaysRepository;
+
+        public ReservationDaysService(
+            IDeletableEntityRepository<ReservationDay, Guid> entityRepository,
+            IMapper mapper)
+            : base(entityRepository, mapper)
+        {
+            this.reservationDaysRepository = entityRepository;
+        }
+
+        /// <inheritdoc/>
+        public async Task<ICollection<ReservationDayDto>> GetAllAsync(QueryOptions<ReservationDayDto>? queryOptions = null)
+        {
+            return await base.GetAllAsync(queryOptions ?? new ());
+        }
+
+        /// <inheritdoc/>
+        public async Task<ReservationDayDto> GetAsync(Guid id, QueryOptions<ReservationDayDto>? queryOptions = null)
+        {
+            return await base.GetAsync(id, queryOptions ?? new());
+        }
+
+        /// <inheritdoc/>
+        public async Task<ICollection<ReservationDayDto>> GetAllForDate(DateOnly date, QueryOptions<ReservationDayDto>? queryOptions = null)
+        {
+            var dtos = await this.reservationDaysRepository
+                .All(queryOptions?.IsReadOnly ?? false, queryOptions?.WithDeleted ?? false)
+                .Include(rd => rd.Room)
+                .ThenInclude(r => r.Type)
+                .Where(rd => rd.Date == date)
+                .ProjectTo<ReservationDayDto>(this.mapper.ConfigurationProvider)
+                .ToListAsync();
+
+            return dtos;
+        }
+
+        /// <inheritdoc/>
+        public async Task<ICollection<ReservationDayDto>> GetAllForPeriod(DateOnly startDate, DateOnly endDate, QueryOptions<ReservationDayDto>? queryOptions = null)
+        {
+            var dtos = await this.reservationDaysRepository
+                .All(queryOptions?.IsReadOnly ?? false, queryOptions?.WithDeleted ?? false)
+                .Include(rd => rd.Room)
+                .ThenInclude(r => r.Type)
+                .Where(rd => rd.Date >= startDate && rd.Date <= endDate)
+                .ProjectTo<ReservationDayDto>(this.mapper.ConfigurationProvider)
+                .ToListAsync();
+
+            return dtos;
+        }
+
+        /// <inheritdoc/>
+        public async Task CreateForReservationAsync(Reservation reservation, int roomId)
+        {
+            bool reservationDaysExist = await this.reservationDaysRepository
+                .All()
+                .AnyAsync(rd => rd.ReservationId == reservation.Id);
+
+            if (reservationDaysExist)
+            {
+                throw new InvalidOperationException("There are reservation days for this reservation already.");
+            }
+
+            int reservationLength = (reservation.DepartureDate.DayNumber - reservation.ArrivalDate.DayNumber) + 1;
+            var reservationDays = new List<ReservationDay>();
+
+            for (int i = 0; i < reservationLength; i++)
+            {
+                var currentDate = new DateOnly(
+                        reservation.ArrivalDate.Year,
+                        reservation.ArrivalDate.Month,
+                        reservation.ArrivalDate.Day);
+                currentDate = currentDate.AddDays(i);
+
+                var reservationDay = new ReservationDay
+                {
+                    ReservationId = reservation.Id,
+                    RoomId = roomId,
+                    Date = currentDate,
+                };
+
+                reservationDays.Add(reservationDay);
+            }
+
+            await this.reservationDaysRepository.AddRangeAsync(reservationDays);
+            await this.reservationDaysRepository.SaveChangesAsync();
+        }
+    }
+}
