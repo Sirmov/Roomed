22cc9ee6a05bc54d08a1bbb610faa84a998d1d95
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-11 20:31:56 +0200

    Update GlobalSuppressions.cs

--- src/Web/Roomed.Web/GlobalSuppressions.cs
+++ src/Web/Roomed.Web/GlobalSuppressions.cs
@@ -1,8 +1,14 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
+﻿// |-----------------------------------------------------------------------------------------------------|
+// <copyright file="GlobalSuppressions.cs" company="Roomed">
+// Copyright (c) Roomed. All Rights Reserved.
+// Licensed under the GPLv3 license. See LICENSE file in the project root for full license information.
+// </copyright>
+// |-----------------------------------------------------------------------------------------------------|
+
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
-
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1100:Do not prefix calls with base unless local implementation exists", Justification = "<Pending>", Scope = "member", Target = "~M:Roomed.Web.Controllers.IdentityDocumentsController.Delete(System.Guid,Roomed.Web.ViewModels.IdentityDocument.IdentityDocumentViewModel)~System.Threading.Tasks.Task{Microsoft.AspNetCore.Mvc.IActionResult}")]
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1100:Do not prefix calls with base unless local implementation exists", Justification = "Using base identifier increases readability of the code.")]
