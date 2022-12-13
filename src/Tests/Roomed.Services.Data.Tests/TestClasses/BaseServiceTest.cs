6b123d302218ebfff1d9933049648c8224e121a5
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-13 19:58:33 +0200

    Fixed file header

--- src/Tests/Roomed.Services.Data.Tests/TestClasses/BaseServiceTest.cs
+++ src/Tests/Roomed.Services.Data.Tests/TestClasses/BaseServiceTest.cs
@@ -1,6 +1,9 @@
-﻿// <copyright file="BaseServiceTest.cs" company="Roomed">
+﻿// |-----------------------------------------------------------------------------------------------------|
+// <copyright file="BaseServiceTest.cs" company="Roomed">
 // Copyright (c) Roomed. All Rights Reserved.
+// Licensed under the GPLv3 license. See LICENSE file in the project root for full license information.
 // </copyright>
+// |-----------------------------------------------------------------------------------------------------|
 
 namespace Roomed.Services.Data.Tests.TestClasses
 {
