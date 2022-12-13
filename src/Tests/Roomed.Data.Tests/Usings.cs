7ab015513a4f1b59e2db9709dfb906fbbbe02e0e
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-13 12:02:05 +0200

    Removed global usings

--- src/Tests/Roomed.Data.Tests/Usings.cs
+++
@@ -1,6 +0,0 @@
-// <copyright file="Usings.cs" company="Roomed">
-// Copyright (c) Roomed. All Rights Reserved.
-// </copyright>
-
-global using NUnit.Framework;
-global using static Roomed.Tests.Common.TestsSetUp;
