91a16ff4a8355576fd85c92fe0d44b09adacb74d
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-14 12:25:23 +0200

    Create GlobalSuppressions.cs

---
+++ src/Services/Roomed.Services.Data/GlobalSuppressions.cs
@@ -0,0 +1,14 @@
+﻿// |-----------------------------------------------------------------------------------------------------|
+// <copyright file="GlobalSuppressions.cs" company="Roomed">
+// Copyright (c) Roomed. All Rights Reserved.
+// Licensed under the GPLv3 license. See LICENSE file in the project root for full license information.
+// </copyright>
+// |-----------------------------------------------------------------------------------------------------|
+
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1100:Do not prefix calls with base unless local implementation exists", Justification = "Using base identifier increases readability of the code.")]
