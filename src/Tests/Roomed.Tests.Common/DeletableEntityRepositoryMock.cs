3ce3edfc3b802db45603caa6b485aac1365b9219
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-14 12:23:40 +0200

    Mocked find and delete async

--- src/Tests/Roomed.Tests.Common/DeletableEntityRepositoryMock.cs
+++ src/Tests/Roomed.Tests.Common/DeletableEntityRepositoryMock.cs
@@ -1,23 +1,38 @@
-﻿namespace Roomed.Tests.Common
+﻿// |-----------------------------------------------------------------------------------------------------|
+// <copyright file="DeletableEntityRepositoryMock.cs" company="Roomed">
+// Copyright (c) Roomed. All Rights Reserved.
+// Licensed under the GPLv3 license. See LICENSE file in the project root for full license information.
+// </copyright>
+// |-----------------------------------------------------------------------------------------------------|
+
+namespace Roomed.Tests.Common
 {
     using Microsoft.EntityFrameworkCore;
     using MockQueryable.Moq;
     using Moq;
+
     using Roomed.Data.Common.Models;
     using Roomed.Data.Common.Repositories;
-    using Roomed.Data.Models;
 
+    /// <summary>
+    /// This class is a mock of <see cref="DeletableEntityRepositoryMock{TEntity, TKey}"/>.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity.</typeparam>
+    /// <typeparam name="TKey">The type of the primary key of the entity.</typeparam>
     public static class DeletableEntityRepositoryMock<TEntity, TKey>
         where TEntity : BaseDeletableModel<TKey>
     {
-        private static readonly ICollection<TEntity> entities = new List<TEntity>();
+        private static readonly ICollection<TEntity> Entities = new List<TEntity>();
 
+        /// <summary>
+        /// Gets the <see cref="DeletableEntityRepositoryMock{TEntity, TKey}"/> instance of the mock.
+        /// </summary>
         public static IDeletableEntityRepository<TEntity, TKey> Instance
         {
             get
             {
                 var mock = new Mock<IDeletableEntityRepository<TEntity, TKey>>();
-                var entitiesMock = entities.BuildMock();
+                var entitiesMock = Entities.BuildMock();
 
                 mock.Setup(m => m.All(It.IsAny<bool>(), It.IsAny<bool>()))
                     .Returns((bool isReadonly, bool withDeleted) =>
@@ -41,8 +56,9 @@
                     .Returns((TEntity entity) =>
                     {
                         entity.CreatedOn = DateTime.UtcNow;
-                        entities.Add(entity);
-                        return null;
+                        Entities.Add(entity);
+
+                        return null!;
                     });
 
                 mock.Setup(m => m.FindAsync(It.IsAny<TKey>(), It.IsAny<bool>()).Result)
@@ -50,7 +66,7 @@
                     {
                         ArgumentNullException.ThrowIfNull(id);
 
-                        TEntity? entity = entities.FirstOrDefault(e => EqualityComparer<TKey>.Default.Equals(e.Id, id));
+                        TEntity? entity = Entities.FirstOrDefault(e => EqualityComparer<TKey>.Default.Equals(e.Id, id));
 
                         if (entity == null)
                         {
@@ -60,16 +76,47 @@
                         return entity;
                     });
 
-                mock.Setup(m => m.Dispose()).Callback(() => entities.Clear());
+                mock.Setup(m => m.Find(It.IsAny<TKey>(), It.IsAny<bool>()))
+                    .Returns((TKey id, bool isReadonly) =>
+                    {
+                        ArgumentNullException.ThrowIfNull(id);
+
+                        TEntity? entity = Entities.FirstOrDefault(e => EqualityComparer<TKey>.Default.Equals(e.Id, id));
+
+                        if (entity == null)
+                        {
+                            throw new InvalidOperationException("There is no entity found with this id!");
+                        }
+
+                        return entity;
+                    });
+
+                mock.Setup(m => m.DeleteAsync(It.IsAny<TKey>()))
+                    .Returns((TKey id) =>
+                    {
+                        var entity = Entities.FirstOrDefault(e => EqualityComparer<TKey>.Default.Equals(e.Id, id));
+
+                        if (entity != null)
+                        {
+                            entity.IsDeleted = true;
+                        }
+
+                        return Task.CompletedTask;
+                    });
+
+                mock.Setup(m => m.Dispose()).Callback(() => Entities.Clear());
 
                 var repository = mock.Object;
                 return repository;
             }
         }
 
+        /// <summary>
+        /// This method clears the internal entities collection.
+        /// </summary>
         public static void Dispose()
         {
-            entities.Clear();
+            Entities.Clear();
         }
     }
 }
