9fb3c769ac0116ac70ff4d52f3e48f34f4823539
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-16 09:50:48 +0200

    Added service mocks

---
+++ src/Tests/Roomed.Tests.Common/ReservationsServiceMock.cs
@@ -0,0 +1,73 @@
+﻿namespace Roomed.Tests.Common
+{
+    using AutoMapper;
+    using Moq;
+
+    using Roomed.Data.Common.Repositories;
+    using Roomed.Data.Models;
+    using Roomed.Services.Data.Common;
+    using Roomed.Services.Data.Contracts;
+    using Roomed.Services.Data.Dtos.Reservation;
+
+    /// <summary>
+    /// This class is a mock of <see cref="IReservationsService"/>.
+    /// </summary>
+    public static class ReservationsServiceMock
+    {
+        private static readonly ICollection<Reservation> Reservations = new List<Reservation>()
+        {
+            new Reservation()
+            {
+                Id = Guid.Parse("a0368b88-05bb-48ff-83cb-0c1c6a323e4e"),
+            },
+            new Reservation()
+            {
+                Id = Guid.Parse("33fec2d6-a85c-40f8-8738-848c68bc2ac8"),
+            },
+        }.AsReadOnly();
+
+        private static IMapper mapper = MapperMock.Instance;
+        private static IDeletableEntityRepository<Reservation, Guid> reservationsRepository = DeletableEntityRepositoryMock<Reservation, Guid>.Instance;
+
+        private static bool isInitialized = false;
+
+        /// <summary>
+        /// Gets the <see cref="IReservationsService"/> instance of the mock.
+        /// </summary>
+        public static IReservationsService Instance
+        {
+            get
+            {
+                if (!isInitialized)
+                {
+                    foreach (var item in Reservations)
+                    {
+                        reservationsRepository.Add(item);
+                    }
+                }
+
+                var mock = new Mock<IReservationsService>();
+
+                mock.Setup(m => m.ExistsAsync(It.IsAny<Guid>(), It.IsAny<QueryOptions<ReservationDto>?>()).Result)
+                    .Returns((Guid id, QueryOptions<ReservationDto>? queryOptions) =>
+                    {
+                        var result = true;
+
+                        try
+                        {
+                            reservationsRepository.Find(id);
+                        }
+                        catch (InvalidOperationException)
+                        {
+                            result = false;
+                        }
+
+                        return result;
+                    });
+
+                var service = mock.Object;
+                return service;
+            }
+        }
+    }
+}
---
+++ src/Tests/Roomed.Tests.Common/RoomsServiceMock.cs
@@ -0,0 +1,96 @@
+﻿// |-----------------------------------------------------------------------------------------------------|
+// <copyright file="RoomsServiceMock.cs" company="Roomed">
+// Copyright (c) Roomed. All Rights Reserved.
+// Licensed under the GPLv3 license. See LICENSE file in the project root for full license information.
+// </copyright>
+// |-----------------------------------------------------------------------------------------------------|
+
+namespace Roomed.Tests.Common
+{
+    using AutoMapper;
+    using Moq;
+
+    using Roomed.Data.Common.Repositories;
+    using Roomed.Data.Models;
+    using Roomed.Services.Data.Common;
+    using Roomed.Services.Data.Contracts;
+    using Roomed.Services.Data.Dtos.Room;
+
+    /// <summary>
+    /// This class is a mock of <see cref="IRoomsService"/>.
+    /// </summary>
+    public static class RoomsServiceMock
+    {
+        private static readonly ICollection<Room> Rooms = new List<Room>()
+        {
+            new Room()
+            {
+                Id = 1,
+                Number = "100",
+                IsDeleted = false,
+            },
+            new Room()
+            {
+                Id = 2,
+                Number = "101",
+                IsDeleted = false,
+            },
+            new Room()
+            {
+                Id = 3,
+                Number = "200",
+                IsDeleted = false,
+            },
+            new Room()
+            {
+                Id = 4,
+                Number = "201",
+                IsDeleted = false,
+            },
+        }.AsReadOnly();
+
+        private static IMapper mapper = MapperMock.Instance;
+        private static IDeletableEntityRepository<Room, int> roomsRepository = DeletableEntityRepositoryMock<Room, int>.Instance;
+
+        private static bool isInitialized = false;
+
+        /// <summary>
+        /// Gets the <see cref="IRoomsService"/> instance of the mock.
+        /// </summary>
+        public static IRoomsService Instance
+        {
+            get
+            {
+                if (!isInitialized)
+                {
+                    foreach (var item in Rooms)
+                    {
+                        roomsRepository.Add(item);
+                    }
+                }
+
+                var mock = new Mock<IRoomsService>();
+
+                mock.Setup(m => m.ExistsAsync(It.IsAny<int>(), It.IsAny<QueryOptions<RoomDto>?>()).Result)
+                    .Returns((int id, QueryOptions<RoomDto>? queryOptions) =>
+                    {
+                        var result = true;
+
+                        try
+                        {
+                            roomsRepository.Find(id);
+                        }
+                        catch (InvalidOperationException)
+                        {
+                            result = false;
+                        }
+
+                        return result;
+                    });
+
+                var service = mock.Object;
+                return service;
+            }
+        }
+    }
+}
