e097fde10b1713a98220a39e9be6329cddb38930
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-17 20:37:23 +0200

    Changed seeding logic

--- src/Web/Roomed.Web/Program.cs
+++ src/Web/Roomed.Web/Program.cs
@@ -117,23 +117,19 @@ internal class Program
 
     private static async Task ConfigurePipelineAsync(WebApplication app)
     {
-        using (var serviceScope = app.Services.CreateScope())
-        {
-            var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-            await ApplicationDbContextSeeder.SeedAsync(dbContext, serviceScope.ServiceProvider);
-        }
-
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
-            app.UseMigrationsEndPoint();
+
+            using (var serviceScope = app.Services.CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                await ApplicationDbContextSeeder.SeedAsync(dbContext, serviceScope.ServiceProvider);
+            }
         }
         else
         {
             app.UseExceptionHandler("/Home/Error");
-
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
 
