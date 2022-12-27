c5e5639e513fdb9c36ba38a097c4e4d094171eeb
Nikola Sirmov <sirmov0213@gmail.com>
2022-12-27 19:19:27 +0200

    Fixed declaration error

--- src/Web/Roomed.Web/wwwroot/js/views/reservations/create.js
+++ src/Web/Roomed.Web/wwwroot/js/views/reservations/create.js
@@ -17,6 +17,6 @@ if (sessionStorage.getItem(storageKey) !== null) {
 createProfileLinkElement.addEventListener('click', (e) => {
     const formData = new FormData(reservationsForm);
     const formEntries = [...formData.entries()];
-    let = data = formEntries.reduce((a, [k, v]) => (allowedData.includes(k) ? Object.assign(a, { [k]: v }) : a), {});
+    let data = formEntries.reduce((a, [k, v]) => (allowedData.includes(k) ? Object.assign(a, { [k]: v }) : a), {});
     sessionStorage.setItem(storageKey, JSON.stringify(data));
 });
