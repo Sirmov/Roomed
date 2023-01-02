69ac6ffbbcac86f38e0d8dbbaef8f93a0d80556a
Nikola Sirmov <sirmov0213@gmail.com>
2023-01-02 21:23:38 +0200

    Changed folder structure to match asp conventions

---
+++ src/Web/Roomed.Web/wwwroot/js/controllers/identityDocuments/create.js
@@ -0,0 +1,22 @@
+﻿const allowedData = ['Type', 'OwnerId', 'NameInDocument', 'Country', 'DocumentNumber', 'PersonalNumber', 'PlaceOfBirth', 'Birthdate', 'IssuedBy', 'ValidFrom', 'ValidUntil'];
+const storageKey = 'createIdentityDocument';
+const createProfileLinkElement = document.getElementById('create-profile-link');
+const identityDocumentForm = document.getElementById('identity-document-form');
+
+if (sessionStorage.getItem(storageKey) !== null) {
+    let data = JSON.parse(sessionStorage.getItem(storageKey));
+    let inputs = document.querySelectorAll('input.form-control');
+
+    inputs.forEach(i => {
+        i.value = data[i.id];
+    });
+
+    sessionStorage.removeItem(storageKey);
+}
+
+createProfileLinkElement.addEventListener('click', (e) => {
+    const formData = new FormData(identityDocumentForm);
+    const formEntries = [...formData.entries()];
+    let data = formEntries.reduce((a, [k, v]) => (allowedData.includes(k) ? Object.assign(a, { [k]: v }) : a), {});
+    sessionStorage.setItem(storageKey, JSON.stringify(data));
+});
---
+++ src/Web/Roomed.Web/wwwroot/js/controllers/identityDocuments/edit.js
@@ -0,0 +1,22 @@
+﻿const allowedData = ['Type', 'OwnerId', 'NameInDocument', 'Country', 'DocumentNumber', 'PersonalNumber', 'PlaceOfBirth', 'Birthdate', 'IssuedBy', 'ValidFrom', 'ValidUntil'];
+const storageKey = 'editIdentityDocument';
+const createProfileLinkElement = document.getElementById('create-profile-link');
+const identityDocumentEditForm = document.getElementById('identity-document-edit-form');
+
+if (sessionStorage.getItem(storageKey) !== null) {
+    let data = JSON.parse(sessionStorage.getItem(storageKey));
+    let inputs = document.querySelectorAll('input.form-control');
+
+    inputs.forEach(i => {
+        i.value = data[i.id];
+    });
+
+    sessionStorage.removeItem(storageKey);
+}
+
+createProfileLinkElement.addEventListener('click', (e) => {
+    const formData = new FormData(identityDocumentEditForm);
+    const formEntries = [...formData.entries()];
+    let data = formEntries.reduce((a, [k, v]) => (allowedData.includes(k) ? Object.assign(a, { [k]: v }) : a), {});
+    sessionStorage.setItem(storageKey, JSON.stringify(data));
+});
